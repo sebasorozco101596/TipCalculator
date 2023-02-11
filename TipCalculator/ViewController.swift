f92bff9e62a337a2991728a17c6e0ab6898c4a90
Juan S Orozco Buitrago <Sebasorozcob@gmail.com>
2023-02-11 09:09:10 -0500

    Initial Commit

---
+++ TipCalculator/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TipCalculator
+//
+//  Created by Juan Sebastian Orozco Buitrago on 2/11/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
